09a9dcee93d8190ec36118d2e5348d73fa86575e
Sushovit204 <shahsushovit434@gmail.com>
2023-03-30 18:01:32 +0545

    set up the DB and now can view movies name by get request

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+import mysql.connector
+import json 
 
 app = FastAPI()
 
-@app.get("/")
-def welcome() -> dict:
-    return {"message":"welcome"}
+#connecting to database
+db = mysql.connector.connect(
+    host ="localhost",
+    user ="root",
+    password = "Gaming.004",
+    database = "imdb_movies"
+)
 
+
+# class Movie(BaseModel):
+#     title:str
+#     watched: bool = False
+
+@app.get("/")
+def welcome():
+    return {"Welcome to my Music Library API.To view all the movies data go to /movies endpoint"}
+
+@app.get("/movies")
+def get_movies_table(response:Response):
+    cursor = db.cursor()
+    query = f"SELECT title FROM MOVIES"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return{"movies":rows}
+
+# @app.get("/post")
+# def creating_favourite(new_movie = Movie)
